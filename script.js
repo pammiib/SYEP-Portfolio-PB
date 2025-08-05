69e8f20b56cfe881cf689e8262cf80b81a4c3460
pammiib <pammi@changefoodforgood.com>
2025-08-05 19:23:19 +0000

    Added a testimonial page

--- script.js
+++ script.js
@@ -1,32 +1,112 @@
-console.log("Hello World");
+const reviews = [
+    {
+        id: 1,
+        name: 'Susan Smith',
+        img: '#',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
 
-let chicken = "123";
-console.log(chicken);
-console.log(typeof chicken);
+       {
+        id: 2,
+        name: 'Anna Smith',
+        img: '#',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
 
-console.log(10 + " eggs");
-console.log(10 + 8 + " eggs");
-console.log("eggs "+ 10 +8);
+       {
+        id: 3,
+        name: 'Peter Jones',
+        img: '#',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+];
 
-//Both Conditions Need to be true
-console.log(7>3)&&(10>8);
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
 
-//Only one condition must be true
-console.log(10>3)||(5>10);
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
 
-//If the statement is logical or not
-console.log(!5>3);
+let currentItem = 0;
+window.addEventListener('DOMContentLoaded', function(){
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+});
 
-
-const hour = new Date().getHours();
-let greeting = "";
-
-if (hour<12){
-    greeting = "Good morning ☀️";
-}else if (hour<18){
-    greeting = "Good afternoon ⛅️";
-}else{
-    greeting = "Good evening 🌙";
+function showPerson(person){
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
 }
 
-document.getElementById("greeting").innerText = greeting;
+nextBtn.addEventListener('click', function(){
+    currentItem++;
+    if (currentItem > reviews.length - 1){
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+
+prevBtn.addEventListener('click', function (){
+    currentItem--;
+    if(currentItem < 0){
+        currentItem = reviews.length -1;
+    }
+    showPerson(currentItem);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Hello World");
+
+// let chicken = "123";
+// console.log(chicken);
+// console.log(typeof chicken);
+
+// console.log(10 + " eggs");
+// console.log(10 + 8 + " eggs");
+// console.log("eggs "+ 10 +8);
+
+// //Both Conditions Need to be true
+// console.log(7>3)&&(10>8);
+
+// //Only one condition must be true
+// console.log(10>3)||(5>10);
+
+// //If the statement is logical or not
+// console.log(!5>3);
+
+
+// const hour = new Date().getHours();
+// let greeting = "";
+
+// if (hour<12){
+//     greeting = "Good morning ☀️";
+// }else if (hour<18){
+//     greeting = "Good afternoon ⛅️";
+// }else{
+//     greeting = "Good evening 🌙";
+// }
+
+// document.getElementById("greeting").innerText = greeting;
